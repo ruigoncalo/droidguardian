e91b7ea927482199fa4877bae2dec15aaf291209
Rui Gonçalo <ruimiguelgoncalo@gmail.com>
2013-10-24 16:52:55 +0100

    add server module

--- sockets/server_module.c
+++ sockets/server_module.c
@@ -1,5 +1,5 @@
 /*
- *  Local domain sockets - Client kernel module
+ *  Local domain sockets - Server kernel module
  *  ruimiguelgoncalo@gmail.com
  *
  *  Relevant information:
@@ -52,50 +52,53 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/un.h>
-#include <linux/net.h>
-#include <net/sock.h>
 #include <linux/socket.h>
+#include <linux/net.h>
+#include <linux/un.h>
+#include <net/sock.h>
 
 #define SOCK_PATH	"/tmp/usocket"
-#define MAX		100
+#define LISTEN		10
 
 struct socket *sock = NULL;
+struct socket *newsock = NULL;
 
-
-static int __init client_module_init( void ) {
+static int __init server_module_init( void ) {
 
   int retval;
-  char str[MAX];
+  char* string = "hello_world";
 
   struct sockaddr_un addr;
   struct msghdr msg;
   struct iovec iov;
   mm_segment_t oldfs;
 
-  printk(KERN_INFO "Start client module.\n");
-
   // create
-  retval = sock_create(AF_UNIX, SOCK_STREAM, 0, &sock); 
-  
-  // connect
-  memset(&addr, 0, sizeof(addr));  
+  retval = sock_create(AF_UNIX, SOCK_STREAM, 0, &sock);
+
+  memset(&addr, 0, sizeof(addr));
   addr.sun_family = AF_UNIX;
   strcpy(addr.sun_path, SOCK_PATH);
 
-  retval = sock->ops->connect(sock, (struct sockaddr *)&addr, sizeof(addr), 0);
+  // bind
+  retval = sock->ops->bind(sock,(struct sockaddr *)&addr, sizeof(addr));
 
-  // recvmsg
+  // listen
+  retval = sock->ops->listen(sock, LISTEN);
 
+  //accept
+  retval = sock->ops->accept(sock, newsock, 0);
+
+  //sendmsg
   memset(&msg, 0, sizeof(msg));
   memset(&iov, 0, sizeof(iov));
 
   msg.msg_name = 0;
   msg.msg_namelen = 0;
   msg.msg_iov = &iov;
+  msg.msg_iov->iov_base = string;
+  msg.msg_iov->iov_len = strlen(string)+1;
   msg.msg_iovlen = 1;
-  msg.msg_iov->iov_base= str;
-  msg.msg_iov->iov_len= strlen(str)+1;
   msg.msg_control = NULL;
   msg.msg_controllen = 0;
   msg.msg_flags = 0;
@@ -103,24 +106,17 @@ static int __init client_module_init( void ) {
   oldfs = get_fs();
   set_fs(KERNEL_DS);
 
-  retval = sock_recvmsg(sock, &msg, strlen(str)+1, 0);
+  retval = sock_sendmsg(newsock, &msg, strlen(string)+1);
 
   set_fs(oldfs);
-
-  // print str
-  printk(KERN_INFO "client module: %s.\n",str);
-
-  // release socket
-  sock_release(sock);
-
+  
   return 0;
 }
 
-static void __exit client_module_exit( void )
-{
-  printk(KERN_INFO "Exit client module.\n");
+static void __init server_module_exit( void ) {
+  printk(KERN_INFO "Exit usocket.");
 }
 
- module_init( client_module_init );
- module_exit( client_module_exit );
- MODULE_LICENSE("GPL");
+module_init( server_module_init );
+module_exit( server_module_exit );
+MODULE_LICENSE("GPL");
